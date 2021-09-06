ccc573b56a5f86e9be9265af6bf1dd5751900db2
Liliana <lily.gallegos@hotmail.com>
2021-09-06 00:46:53 -0500

    div resize

--- src/Components/CanvasParent/CanvasParent.jsx
+++ src/Components/CanvasParent/CanvasParent.jsx
@@ -3,17 +3,25 @@ import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import { Environment } from '@react-three/drei'
 import Model from '../../Models/Taza';
+import Background from '../../Models/fondo.jpg';
+import './CanvasParent.css'
+
 export default function CanvasParent() {
+
+  var bg = require('../../Models/fondo.jpg');
   return (
+    <div className="full-height">
     <Canvas
-      className="canvasParent"
       shadows
       concurrentcamera={{ position: [0, 0, 4], fov: 50 }}
     >
+       <ambientLight intensity={0.2} />
+      <directionalLight />
       <Suspense fallback={null}>
         <Model />
         <Environment preset="sunset" background></Environment>
       </Suspense>
     </Canvas>
+    </div>
   );
 }
